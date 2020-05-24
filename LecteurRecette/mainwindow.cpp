b9971d194c7792d87037fbb56f1f1b38dc7504e0
mattduf13 <57948224+mattduf13@users.noreply.github.com>
2020-05-24 16:04:54 +0200

    Titre fenêtre principale

--- LecteurRecette/mainwindow.cpp
+++ LecteurRecette/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowTitle("Lecteur de recettes");//titre de la fenêtre
 }
 
 MainWindow::~MainWindow()
