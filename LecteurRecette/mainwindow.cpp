fa1472f5d785efa0988d6397fee0ecc9d7f25838
mattduf13 <57948224+mattduf13@users.noreply.github.com>
2020-05-24 16:28:26 +0200

    Titre de la fenêtre

--- LecteurRecette/mainwindow.cpp
+++ LecteurRecette/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowTitle("Lecteur de recettes");
+
     QMenu *fileMenu = new QMenu(tr("Fichier"), this);
     zonetexte = new QTextEdit;
     setCentralWidget(zonetexte);
