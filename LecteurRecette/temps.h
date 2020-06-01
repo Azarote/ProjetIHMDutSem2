c04dd12d6dd287b2f48027d7d2abad846f103862
Azarote <matteo_munoz@hotmail.fr>
2020-06-01 22:48:30 +0200

    Update temps.h

--- LecteurRecette/temps.h
+++ LecteurRecette/temps.h
@@ -13,6 +13,15 @@
 class traitement
 {
 public:
+    /**
+     * \fn Traitement temps pour affichage
+     * \brief Fonction qui calcul le temps
+     * \showrefs
+     *
+     * \param Tous les QStringList en passage par adresse qui récupère le temps
+     * \param instance de la classe LecteurJson qui contient les temps.
+     * \return Modifie les variable qui contient maintenant le temps sous forme compréhensible.
+     */
     void traitementTemps(QStringList &, QStringList&, QStringList&, LecteurJson);
 };
 
