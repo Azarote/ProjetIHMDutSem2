fa124108ca1fec51073362aabe2db54095b3353a
mattduf13 <57948224+mattduf13@users.noreply.github.com>
2020-06-02 23:10:15 +0200

    Ajustements mineurs sur les commentaires existants + ajout de nouveaux commentaires dans mainWindow.h

--- LecteurRecette/lecteurjson.h
+++ LecteurRecette/lecteurjson.h
@@ -17,7 +17,7 @@
 #include <QStringListModel>
 
 /*! \class LecteurJson
-   * \brief classe qui permet la lecture d'un fichier JSON
+   * \brief Classe qui permet la lecture d'un fichier JSON
    *
    */
 class LecteurJson
@@ -28,24 +28,18 @@ private:
 public:
     /**
      * \fn Lecteur
-     * \brief Fonction qui lis le fichier Json.
+     * \brief Fonction qui lit le fichier Json.
      *
      * \param QString qui contient le chemin du fichier.
      */
     void Lecteur(QString );
 
-    /**
-     * \fn Lancementlecture
-     * \brief Fonction qui appelle la fonction qui traite le json + ouvre toutes les fenêtres nécéssaires.
-     *
-     * \param QString qui contient le chemin du fichier(reçue par la fonction ouvrir).
-     */
     inline QStringList getInfosRecette(){ return InfosRecette;}/*!< QStringList qui retourne les infos de la recette*/
     inline QStringList getTemps(){ return Temps;}/*!< QStringList qui retourne les temps de la recette*/
     inline QStringList getIngredients(){ return Ingredients;}/*!< QStringList qui retourne les ingredients de la recette*/
-    inline QStringList getEtapes(){ return Etapes;}/*!< QStringList qui retourne les etapes de la recette*/
+    inline QStringList getEtapes(){ return Etapes;}/*!< QStringList qui retourne les étapes de la recette*/
     inline QString getUpdateEtapes(int i){ return Etapes.at(i);}/*!< QString qui retourne le numéro de l'étape*/
-    inline QStringList getURL(){ return URL;}/*!< QStringList qui retourne l'URL de la recette */
+    inline QStringList getURL(){ return URL;}/*!< QStringList qui retourne l'URL de la recette*/
 };
 
 #endif // LECTEURJSON_H
